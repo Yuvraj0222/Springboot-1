0cbf7a22cda3a57c122f2a1c13d577859c2208aa
Yuvraj0222 <yuvrajhardoi@gmail.com>
2026-07-17 10:45:47 +0530

    Student with database

--- src/main/java/com/yuv/demo/StudebtServer/StudentRepository.java
+++ src/main/java/com/yuv/demo/StudebtServer/StudentRepository.java
@@ -1,11 +1,13 @@
-package com.yuv.demo.StudentServer;
+package com.yuv.demo.StudebtServer;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class StudentRepository {
-    public Student save(Student student){
-        System.out.println("Student information saved");
-        return student;
-    }
+
+public interface StudentRepository extends JpaRepository<com.yuv.demo.StudentServer.Student,Integer>{
+
+//    public Student save(Student student){
+//        System.out.println("Student information saved");
+//        return student;
+//    }
 }
